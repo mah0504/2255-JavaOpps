e8ae15a354576fa82cca4a8420132a3da80f7682
maha amhaouch <152235666+mah0504@users.noreply.github.com>
2024-06-13 19:05:50 -0400

    Add files via upload
    prototype

---
+++ Dump/folder/Robot.java
@@ -0,0 +1,12 @@
+public class Robot {
+    private Object[] metriques;
+
+
+
+    public boolean verifierComposantes(){
+        return false; 
+
+        // hashmap ? methode pr checker jsp 
+    }
+
+}
