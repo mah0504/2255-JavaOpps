1c42caaaaaf83551d16fec90eb834e69c4728120
mah0504 <maha.amhaouch@umontreal.ca>
2024-07-03 16:46:33 -0400

    cc

--- prototype/Activités.java
+++ prototype/Activités.java
@@ -3,7 +3,8 @@ public class Activités {
     private HashMap<String , String> activités =new HashMap<String , String>();
 
     // nom de l'activité , type 
-
+  // allo cc
+    // essai num 1 de commit piush ?
 
 
 }
