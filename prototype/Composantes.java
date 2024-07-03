4530a3ea2d0aad0847b79315bb21039610098dcd
mah0504 <maha.amhaouch@umontreal.ca>
2024-07-03 16:54:09 -0400

    cc

--- prototype/Composantes.java
+++ prototype/Composantes.java
@@ -3,15 +3,20 @@ public class Composantes {
 
     public Composantes() {
         this.composantes = new String[]{ "CPU", "Roue", "Bras", "Hélice", "Caméra", "Haut-parleur", "Micro", "Écran"};
-
-
-        // on crée des tableaux de composantes dans fournisseur et utilisateur et on les remplie avec les 
-        // elements de celui-là selon les bsns choix blabla
     }
 
     // Getter pour accéder au tableau
     public String[] getComposantes() {
         return composantes;
-    } 
-}
+    }
 
+    // Une méthode pour imprimer un message de test
+    public void printEssai() {
+        System.out.print("essai1");
+    }
+
+    public static void main(String[] args) {
+        Composantes composantes = new Composantes();
+        composantes.printEssai();
+    }
+}
