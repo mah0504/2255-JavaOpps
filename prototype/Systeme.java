52d33774c3be3aabe6fb500d5242edac65e5ccc3
Audrey <audreymlvo@outlook.com>
2024-07-06 16:48:32 -0400

    Gestion des données

--- prototype/Systeme.java
+++ prototype/Systeme.java
@@ -1,11 +1,44 @@
 import java.util.*;
 
+// Gestion des données stockées et maintenir les listes qui les contiennent.
+
 public class Systeme {
 
-    Action actions= new Action();
+    private static Systeme instance;
+    private ArrayList<Utilisateur> utilisateurs;
+    private ArrayList<Fournisseur> fournisseurs;
 
-    private void selectionnerActions( ){
-
+    private Systeme() {
+        utilisateurs = new ArrayList<>();
+        fournisseurs = new ArrayList<>();
     }
+
+    // Utilise le modèle Singleton pour une unique instance du Systeme.
+    public static Systeme getInstance() {
+        if (instance == null) {
+            instance = new Systeme();
+        }
+        return instance;
+    }
+
+    // Utilisateurs du système
+    public void ajouterUtilisateur(Utilisateur utilisateur) {
+        utilisateurs.add(utilisateur);
+    }
+
+    public ArrayList<Utilisateur> getUtilisateurs() {
+        return utilisateurs; // return new ArrayList<>(utilisateurs); pour copie qui ne peut pas être modifiée???
+    }
+
+    // Fournisseurs du système
+    public void ajouterFournisseur(Fournisseur fournisseur) {
+        fournisseurs.add(fournisseur);
+    }
+
+    public ArrayList<Fournisseur> getFournisseurs() {
+        return fournisseurs;
+    }
+
+
 
 }
