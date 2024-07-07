cae14e6acc88da5891c883fe4d961f10bb3c1058
mah0504 <maha.amhaouch@umontreal.ca>
2024-07-07 12:51:19 -0400

    AfficherMetriques()

--- prototype/Metrique.java
+++ prototype/Metrique.java
@@ -2,8 +2,18 @@ public class Metrique {
 
     private boolean disponibilite;
     private float tauxSuccessTaches;
+    private int performance;
 
 
+    // getters et setters
+    public void setDisponibilite(boolean disponibilite){this.disponibilite=disponibilite;}
+    public void setPerformance(int performance){ this.performance=performance;}
+    public void setTauxSuccessTaches( float tauxSuccessTaches){ this.tauxSuccessTaches=tauxSuccessTaches;}
+
+    public boolean getDisponibilite(){ return disponibilite;}
+
+    public int getPerformance() {return performance;}
+    public float getTauxSuccessTaches(){ return tauxSuccessTaches;}
 
 
 
