934f8d7333399dbc491b5d3dcee8a9c97f5cece1
micha <chaimaa.dannane@umontral.ca>
2024-07-30 11:50:10 -0400

    Update Fournisseur.java

---
+++ src/main/java/Fournisseur.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+
+public class Fournisseur extends Compte{
+
+    private String nomCompagnie;
+
+    /**
+     * Une nouvelle instance de la classe Fournisseur (initialisation par défaut)
+     */
+    public Fournisseur(){
+    }
+
+    /**
+     * Une nouvelle instance de la classe Fournisseur
+     *
+     * @param nomCompagnie le nom de la Compagnie du Fournisseur
+     */
+    public Fournisseur(String nomCompagnie){
+        this.nomCompagnie = nomCompagnie;
+    }
+
+
+    /**
+     * Récupère le nom de la Compagnie
+     *
+     * @return nomCompagnie Le nom de la Compagnie du Fournisseur
+     */
+    public String getNomCompagnie(){
+        return nomCompagnie;
+    }
+
+    /**
+     *  Assigne un nom de Compagnie du Fournisseur
+     *
+     * @param nomCompagnie Le nouveau nom de la Compagnie
+     */
+    public void setNomCompagnie(String nomCompagnie){
+        this.nomCompagnie = nomCompagnie;
+    }
+}
