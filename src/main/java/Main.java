8be00b1cedd0364c171445ce0f078ba5d502ad84
micha <chaimaa.dannane@umontral.ca>
2024-08-06 11:38:07 +0100

    update main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,12 +8,13 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        ControllerUtilisateur controllerUtilisateur = new ControllerUtilisateur();
-        ControllerFournisseur controllerFournisseur = new ControllerFournisseur();
-        ControllerRobot controllerRobot= new ControllerRobot();
-    //    MenuUtilisateur menuUtilisateur = new MenuUtilisateur();
+        //ControllerUtilisateur controllerUtilisateur = new ControllerUtilisateur();
+        //ControllerFournisseur controllerFournisseur = new ControllerFournisseur();
+        //ControllerRobot controllerRobot= new ControllerRobot();
+        //MenuUtilisateur menuUtilisateur = new MenuUtilisateur();
 
-        MenuCompte menuCompte = new MenuCompte(controllerUtilisateur, controllerFournisseur ,controllerRobot);
+        View view = new View();
+        MenuCompte menuCompte = new MenuCompte(view);
         menuCompte.afficherMenu();
 
     }
